718bebc9fe78adfcad5af6f7691065d2397cb0e0
Sankalp Shukla <sankalpshukla@Sankalps-MacBook-Air.local>
2020-12-28 13:54:42 +0530

    inline styling

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import './App.css';
 function App() {
   const date = new Date()
   const hours=date.getHours()
-
+  const styles={ fontSize : 50  }
   let timeOfDay
 
   if(hours>0&&hours<12)
-  {timeOfDay="Morning"}
+  {timeOfDay="Morning"
+styles.color = "#7fffd4"}
   else if(hours>=12&&hours<=20)
-  {timeOfDay="Afternoon"}
-  else {timeOfDay="Night"}
+  {timeOfDay="Afternoon"
+styles.color = "#ff7f50"}
+  else {timeOfDay="Night"
+styles.color = "daa520"}
 
-  return(<h1>Good {timeOfDay}!!</h1>)
+  return(<h1 style={styles}>Good {timeOfDay}!!</h1>)
 }
 
 export default App;
